d6a1ceb227e3021a5b3cc65241003dd1e86b6184
leiwang168 <66875428+leiwang168@users.noreply.github.com>
2024-11-14 10:38:51 +0800

    Add files via upload

---
+++ npmConfig.js
@@ -0,0 +1,16 @@
+//存放私库包的命名空间
+export const scopes = [
+  '@cz','@syl'
+];
+/****
+ * 私库地址，代理端口会解析url的端口号
+ * const privateNpmRegistryURLArr = privateNpmRegistryURL.split(":");
+ * const privateNpmPort = privateNpmRegistryURLArr[privateNpmRegistryURLArr.length - 1]
+ * 拉取一些npm的包会返回302的情况，unpkg暂时没有处理，会不会和本地的npm源有关？
+ ***/
+export const privateNpmRegistryURL = 'http://192.168.1.100:4873';
+
+//互联网npm地址
+export const publicNpmRegistryURL = 'http://registry.npmjs.org';
+
+export default scopes;
